f6dcd7ba3d28178a75e8f3eeb6bd0c352a0ba7b6
elgalloloco <zreggert@gmail.com>
2021-04-20 14:53:25 -0400

    added all the parameters to the constructors of each class as well as the methods that will be used in each

---
+++ index.js
@@ -0,0 +1,15 @@
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {}
+
+    getId() {}
+
+    getEmail() {}
+
+    getRole() {}
+}
